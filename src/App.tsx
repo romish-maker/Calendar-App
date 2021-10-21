ed9a9e8bb3d8ccaeaa486c673ae16108b691ef60
Romish Kuvatov <romish2020@list.ru>
2021-10-21 16:42:29 +0600

    save user to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
+import {useDispatch} from 'react-redux';
 import './App.css';
 import {AppRouter} from './components/AppRouter';
 import {Layout} from 'antd';
 import {Navbar} from './components/Navbar';
+import {AuthActionCreators} from './store/reducers/auth/action-creators';
+import { IUser } from './models/IUser';
 
 export function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      dispatch(AuthActionCreators.setUser({username: localStorage.getItem('username' || '')} as IUser));
+      dispatch(AuthActionCreators.setIsAuth(true));
+    }
+  }, [])
   return (
       <Layout>
         <Navbar/>
